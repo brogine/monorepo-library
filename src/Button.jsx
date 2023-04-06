fe13080e0cb9abecef1c148c7a8f41623142ce97
Eric Brogin <ericbrogin@gmail.com>
2023-04-06 15:14:45 -0300

    Updates.

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import MUIButton from '@mui/material/Button';
+
+export default function Button() {
+  return (
+    <div>
+      <MUIButton variant="contained">Hello World</MUIButton>
+    </div>
+  );
+}
+
